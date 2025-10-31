bab3e25f32711585d1c08c4eead95cdd76301c1f
lohan-g15 <lohanpinheiro9@gmail.com>
2025-10-31 10:43:00 -0300

    Adicionando demais elementos no projeto

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ temaEscuro(temasalvo === 'escuro');
 function temaEscuro(tipo) {
   if (tipo == true) {
     body.classList.add('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    botao.innerHTML = '<span class="material-icons">dark_mode</span>';
   } else {
     body.classList.remove('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    botao.innerHTML = '<span class="material-icons">wb_sunny</span>';
   }
 }
 
